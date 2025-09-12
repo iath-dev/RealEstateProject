7fb6373d1fd9b7251d1181eba30d5d01bffa7722
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-09-12 15:42:08 -0500

    docs: swagger configuration

--- backend/src/RealEstate.API/Program.cs
+++ backend/src/RealEstate.API/Program.cs
@@ -2,40 +2,52 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+// Swagger Configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "RealEstate API",
+            Version = "v1",
+            Description = "API for RealEstate application",
+            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+            {
+                Name = "Daniel Neira",
+                Email = "jdnl95@gmail.com",
+                Url = new Uri("https://danielneira-developer.vercel.app/"),
+            },
+        }
+    );
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "RealEstate API V1");
+        options.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+        options.DisplayRequestDuration(); // Optional: Display request duration
+        options.EnableTryItOutByDefault(); // Optional: Enable "Try it out" by default
+        options.EnableDeepLinking(); // Optional: Enable deep linking
+        options.ShowExtensions();
+    });
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.UseStaticFiles();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
